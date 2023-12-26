9c888f6457de6aac7a286110b1115aa0959b15b7
thelight0804 <thelight0804@gmail.com>
2023-12-26 19:19:12 +0900

    Add: 도시 버튼

--- Korea-weather-React/src/App.jsx
+++ Korea-weather-React/src/App.jsx
@@ -1,11 +1,19 @@
-import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+import {Button} from 'react-bootstrap';
+
+import './App.css';
 
 function App() {
+  const cities = ['서울', '인천', '강릉', '춘천', '대전', '김천', '대구', '광주', '목포', '부산', '울산', '여수', '창원']
+  // const [city, setCity] = useState(cities[0]);
+
   return (
     <div>
-      <Button variant="primary">Primary</Button>
+      {cities.map((city, index) => {
+        return <Button variant="primary" className="city-button" key={index}>{city}</Button>
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
